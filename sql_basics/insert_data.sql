a259c4a52f9fb8e9838bb986924813dae9183dcd
Pro <aadishresth@gmail.com>
2026-06-13 14:20:27 +0530

    Feeding Data

---
+++ sql_basics/insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customers (first_name, last_name, email) 
+VALUES 
+    ('John', 'Doe', 'john.doe@email.com'),
+    ('Jane', 'Smith', 'jane.smith@email.com'),
+    ('Alex', 'Jones', 'alex.jones@email.com');
